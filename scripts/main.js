5aa6135481368ae113de86ecd98f9fde492d0341
Rudy Gonzalez <xrgonzalezx@gmail.com>
2017-07-15 21:52:50 -0400

    second version

--- scripts/main.js
+++ scripts/main.js
@@ -1,28 +1,36 @@
 /*
    Implement a Blackjack hand value calculator.
-
    Open up the `index.html` file and your console
    to watch the assertions pass as you write your code.
-
    Also remember, that the parameter `hand` will be an array, so
    you'll need to parse through that first before you can start to
-   write your logic.
+   write your logic. kj64a
 */
 
-function handValue (hand) {
-  let total = 0;
-  for (let i=0; i < hand.length; i++){
-    let card = hand[i];
-    if(card === "J" || card === "Q" || card === "K") {
-        total += 10;
+function handValue(hand) {
+  let output = 0;
+  for (var i = 0; i < hand.length; i++) {
+    if (hand[i] === "K" || hand[i] === "Q" || hand[i] === "J") {
+      output += 10;
+    } else if (hand[i] === "A") {
+        output += 1;
+      }
+      if (parseInt(card) > 1 || parseInt(card) < 11){
+        total += parseInt(card);
+      }
+      else if ( parseInt(hand[i]) <= 10) {
+      output += parseInt(hand[i]);
     }
-    if (parseInt(card) > 1 || parseInt(card) < 11){
-     total += parseInt(card);
-   }
-    // if ( card === "A" && total <= 10 )
-    //  total += 11
-    //
-  }
+    if (hand[i] === "A" && output <= 11) {
+      output += 10;
+    }
 
-  return total;
+  }
+  return output;
 }
+
+/* -----  Hints ------
+1..10   ==> Worth face value (1 = 1, 4 = 4, etc)
+K, Q, J ==> Worth 10
+A       ==> Worth 1 or 11
+*/
